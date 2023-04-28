2c2e3f8da6601d516059fe125a82685a8cbed828
DavideFarci <yank92.df@gmail.com>
2023-04-28 16:16:45 +0200

    add milestones 0, 1, 2

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,56 @@ Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 */
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const eleMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+for (i = 0; i < eleMembers.length; i++) {
+    console.log(eleMembers[i]);
+};
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+const eleMembersList = document.querySelector(".members");
+
+for (i = 0; i < eleMembers.length; i++) {
+    let member = eleMembers[i];
+    eleMembersList.innerHTML += `<li>${member.name}, ${member.role}, ${member.photo}</li>`;
+};
